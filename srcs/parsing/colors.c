de7cb836e3d3656ea6469ede61a963b50901b678
4yuub <karafi.work@gmail.com>
2022-04-10 22:12:20 +0000

    feat: check for valid color

--- srcs/parsing/colors.c
+++ srcs/parsing/colors.c
@@ -6,7 +6,7 @@
 /*   By: akarafi <akarafi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 05:39:39 by ayoub             #+#    #+#             */
-/*   Updated: 2022/04/10 22:04:49 by akarafi          ###   ########.fr       */
+/*   Updated: 2022/04/10 22:11:54 by akarafi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,25 +22,20 @@ static void	_init(t_data *data)
 	data->ceil_color.b = -1;
 }
 
-static void	get_color_of(int id, char *value, t_data *data, int *error)
+static void	get_color_of(char *value, t_color *c, int *error)
 {
-	t_color	*c;
 	int		n;
 
-	if (id == F)
-		c = &data->floor_color;
-	else if (id == C)
-		c = &data->ceil_color;
 	n = 0;
 	while (!*error && *value)
 	{
 		if (*value == ',' || *value == '\n')
 		{
-			if (c->r == -1)
+			if (c->r == -1 && n <= 255)
 				c->r = n;
-			else if (c->g == -1)
+			else if (c->g == -1 && n <= 255)
 				c->g = n;
-			else if (c->b == -1)
+			else if (c->b == -1 && n <= 255)
 				c->b = n;
 			else
 				*error = INVALID_DATA_ERR;
@@ -69,8 +64,10 @@ void	get_colors(t_list *lst, t_data *data, int *error)
 			*error = DUPLICATED_DATA_ERR;
 		if (id == -1 || *error)
 			break ;
-		if (id == F || id == C)
-			get_color_of(id, value, data, error);
+		if (id == F)
+			get_color_of(value, &data->floor_color, error);
+		else if (id == C)
+			get_color_of(value, &data->ceil_color, error);
 		lst = lst->next;
 	}
 	if (!*error && (data->ceil_color.b == -1 || data->floor_color.b == -1))
