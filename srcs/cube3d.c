f1e7d8b59084a96e95f496e9c1abb9267a6ea044
4yuub <karafi.work@gmail.com>
2022-04-18 00:48:23 +0000

    fix: memory leaks and protection

--- srcs/cube3d.c
+++ srcs/cube3d.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cube3d.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: akarafi <akarafi@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ayoub <ayoub@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/03 22:10:32 by akarafi           #+#    #+#             */
-/*   Updated: 2022/04/17 17:32:59 by akarafi          ###   ########.fr       */
+/*   Updated: 2022/04/18 00:47:37 by ayoub            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,6 +49,7 @@ int	main(int ac, char **av)
 	if (ac != 2)
 		return (printf("%sInvalid number of args%s\n", RED, RESET), 0);
 	error = 0;
+	data = NULL;
 	len = _strlen(av[1]);
 	if (len < 4 || !_strcmp(av[1] + len - 4, av[1] + len, ".cub"))
 		error = FILE_EXTENTION_ERR;
@@ -57,8 +58,8 @@ int	main(int ac, char **av)
 	if (error)
 	{
 		printf("An error have been occured: %s\n", str_error(error));
-		//free_data(data);
+		free_data(data);
 		return (0);
 	}
-	//free_data(data);
+	free_data(data);
 }
--- srcs/free_utils/free_data.c
+++ srcs/free_utils/free_data.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   free_data.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: akarafi <akarafi@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ayoub <ayoub@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 18:34:04 by akarafi           #+#    #+#             */
-/*   Updated: 2022/04/11 18:45:39 by akarafi          ###   ########.fr       */
+/*   Updated: 2022/04/18 00:46:33 by ayoub            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,14 @@
 
 void	free_data(t_data *data)
 {
+	int	i;
+
+	if (!data)
+		return ;
+	i = -1;
+	while (data->map && ++i < data->height)
+		free(data->map[i]);
+	free(data->map);
 	free(data->east_texture);
 	free(data->west_texture);
 	free(data->north_texture);
