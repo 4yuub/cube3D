f5e46307aa9dc1a9b383b0a284d6e3150d5515ea
4yuub <karafi.work@gmail.com>
2022-05-26 02:48:02 +0100

    style: norm error

--- srcs/rendering/minimap.c
+++ srcs/rendering/minimap.c
@@ -6,11 +6,30 @@
 /*   By: akarafi <akarafi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 06:42:18 by akarafi           #+#    #+#             */
-/*   Updated: 2022/05/25 00:42:58 by akarafi          ###   ########.fr       */
+/*   Updated: 2022/05/26 02:47:13 by akarafi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube3d.h"
+
+static char	get_type(t_utils *utils, t_vector_i step, int i, int j)
+{
+	char	c;
+
+	if ((j / step.x) >= utils->data->width \
+					|| (i / step.y) >= utils->data->height)
+		c = EMPTY;
+	else
+	{
+		c = utils->data->map[(i / step.y)][(j / step.x)];
+		if (utils->pos.x - ((double)j / step.x) < .3 \
+			&& utils->pos.x - ((double)j / step.x) >= -.3 \
+			&& utils->pos.y - ((double)i / step.y) >= -.3
+			&& utils->pos.y - ((double)i / step.y) < .3)
+			c = PLAYER;
+	}
+	return (c);
+}
 
 void	draw_minimap(t_utils *utils)
 {
@@ -27,18 +46,7 @@ void	draw_minimap(t_utils *utils)
 		j = -1;
 		while (++j < MINI_WIDTH)
 		{
-			if ((j / step.x) >= utils->data->width \
-				|| (i / step.y) >= utils->data->height)
-				c = EMPTY;
-			else
-			{
-				c = utils->data->map[(i / step.y)][(j / step.x)];
-				if (utils->pos.x - ((double)j / step.x) < .3 \
-					&& utils->pos.x - ((double)j / step.x) >= -.3 \
-					&& utils->pos.y - ((double)i / step.y) >= -.3
-					&& utils->pos.y - ((double)i / step.y) < .3)
-					c = PLAYER;
-			}
+			c = get_type(utils, step, i, j);
 			if (c == PLAYER)
 				utils->minimap.data[i * MINI_WIDTH + j] = 0x00ff00U;
 			else if (c == SPACE || c == EMPTY)
